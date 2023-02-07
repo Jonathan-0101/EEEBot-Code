fcfa19b79f79ece47f876401042d5536347ac539
Jonathan-0101 <jonathan.woolf.01@gmail.com>
2023-02-07 20:25:19 +0000

    Adding comments to nodeRed code

--- Week 3/ESP32 nodeRed full controll/ESP32 nodeRed full controll.ino
+++ Week 3/ESP32 nodeRed full controll/ESP32 nodeRed full controll.ino
@@ -1,4 +1,3 @@
-
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Wire.h>
@@ -6,12 +5,13 @@
 #include <MPU6050_tockn.h>
 #include <iostream>
 #include <string>
+
 using namespace std;
 #define FALSE = 0
 #define TRUE = !FALSE
 #define SOUND_SPEED 0.034
 #define CM_TO_INCH 0.393701
-#define I2C_SLAVE_ADDR 0x04  // 4 in hexadecimal
+#define I2C_SLAVE_ADDR 0x04 // 4 in hexadecimal
 
 MPU6050 mpu6050(Wire);
 
@@ -25,12 +25,11 @@ int rightSpeed;
 int steeringAngle;
 int data[3];
 
-// Replace the next variables with your SSID/Password combination
+// Network credentials
 const char *ssid = "tonyTable";
 const char *password = "brazillostlol";
 
-// Add your MQTT Broker IP address, example:
-// const char* mqtt_server = "192.168.1.144";
+// MQTT server address
 const char *mqtt_server = "192.168.137.115";
 
 WiFiClient espClient;
@@ -39,48 +38,42 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-// uncomment the following lines if you're using SPI
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
-
 float temperature = 0;
 
-// LED Pin
+// LED pins
 const int reverseLights = 23;
 const int leftIndicators = 19;
 const int rightIndicators = 33;
 
-void setup() {
+// Setting up the script
+void setup()
+{
   Serial.begin(9600);
-  // default settings
-  // (you can also pass in a Wire library object like &Wire2)
-  // status = bme.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  pinMode(trigPin, OUTPUT);  // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT);   // Sets the echoPin as an Input
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   pinMode(reverseLights, OUTPUT);
   pinMode(leftIndicators, OUTPUT);
   pinMode(rightIndicators, OUTPUT);
   Wire.begin();
 }
 
-void setup_wifi() {
+// WiFi connection setup
+void setup_wifi()
+{
   delay(10);
-  // We start by connecting to a WiFi network
   Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -91,68 +84,92 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-void callback(char *topic, byte *message, unsigned int length) {
+// Funtion to act on data received from MQTT server (Node-RED)
+void callback(char *topic, byte *message, unsigned int length)
+{
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
   String messageTemp;
 
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)message[i]);
     messageTemp += (char)message[i];
   }
   Serial.println();
 
-  // Feel free to add more if statements to control more GPIOs with MQTT
-
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off".
-  // Changes the output state according to the message
-
-  if (String(topic) == "esp32/allLights") {
+  // Turning all the lights on/off
+  if (String(topic) == "esp32/allLights")
+  {
     Serial.print("Changing output to ");
-    if (messageTemp == "true") {
+    if (messageTemp == "true")
+    {
       Serial.println("on");
       digitalWrite(reverseLights, HIGH);
       digitalWrite(leftIndicators, HIGH);
       digitalWrite(rightIndicators, HIGH);
-    } else if (messageTemp == "false") {
+    }
+    else if (messageTemp == "false")
+    {
       Serial.println("off");
       digitalWrite(reverseLights, LOW);
       digitalWrite(leftIndicators, LOW);
       digitalWrite(rightIndicators, LOW);
     }
   }
-  if (String(topic) == "esp32/leftIndicators") {
+
+  // Turning the left indicator lights on/off
+  if (String(topic) == "esp32/leftIndicators")
+  {
     Serial.print("Changing output to ");
-    if (messageTemp == "true") {
+    if (messageTemp == "true")
+    {
       Serial.println("on");
       digitalWrite(leftIndicators, HIGH);
-    } else if (messageTemp == "false") {
+    }
+    else if (messageTemp == "false")
+    {
       Serial.println("off");
       digitalWrite(leftIndicators, LOW);
     }
   }
-  if (String(topic) == "esp32/rightIndicators") {
+
+  // Turning the right indicator lights on/off
+  if (String(topic) == "esp32/rightIndicators")
+  {
     Serial.print("Changing output to ");
-    if (messageTemp == "true") {
+    if (messageTemp == "true")
+    {
       Serial.println("on");
       digitalWrite(rightIndicators, HIGH);
-    } else if (messageTemp == "false") {
+    }
+    else if (messageTemp == "false")
+    {
       Serial.println("off");
       digitalWrite(rightIndicators, LOW);
     }
   }
-  if (String(topic) == "esp32/reverseLights") {
+
+  // Turning the reverse lights on/off
+  if (String(topic) == "esp32/reverseLights")
+  {
     Serial.print("Changing output to ");
-    if (messageTemp == "true") {
+    if (messageTemp == "true")
+    {
       Serial.println("on");
       digitalWrite(reverseLights, HIGH);
-    } else if (messageTemp == "false") {
+    }
+    else if (messageTemp == "false")
+    {
       Serial.println("off");
       digitalWrite(reverseLights, LOW);
     }
   }
-  if (String(topic) == "esp32/speed") {
+
+  // Setting the speed of the car
+  if (String(topic) == "esp32/speed")
+  {
     leftSpeed = messageTemp.toInt();
     rightSpeed = messageTemp.toInt();
     Wire.beginTransmission(I2C_SLAVE_ADDR);
@@ -164,7 +181,10 @@ void callback(char *topic, byte *message, unsigned int length) {
     Wire.write((byte)(steeringAngle & 0x000000FF));
     Wire.endTransmission();
   }
-  if (String(topic) == "esp32/steeringAngle") {
+
+  // Setting the steering angle of the car
+  if (String(topic) == "esp32/steeringAngle")
+  {
     steeringAngle = messageTemp.toInt();
     Wire.beginTransmission(I2C_SLAVE_ADDR);
     Wire.write((byte)((leftSpeed & 0x0000FF00) >> 8));
@@ -177,14 +197,18 @@ void callback(char *topic, byte *message, unsigned int length) {
   }
 }
 
-void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
+void reconnect()
+{
+  // Loop until reconnected to MQTT server
+  while (!client.connected())
+  {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client"))
+    {
       Serial.println("connected");
-      // Subscribe
+
+      // Subscribing to the topics
       client.subscribe("esp32/allLights");
       client.subscribe("esp32/reverseLights");
       client.subscribe("esp32/leftIndicators");
@@ -196,8 +220,12 @@ void reconnect() {
       rightSpeed = 0;
       dtostrf(107, 1, 2, tempString);
       Serial.println(tempString);
+
+      // Publish the initial speed and steering angle
       client.publish("esp32/initialSteering", tempString);
-    } else {
+    }
+    else
+    {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
@@ -207,28 +235,24 @@ void reconnect() {
   }
 }
 
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
   mpu6050.update();
   long now = millis();
-  if (now - lastMsg > 5000) {
+  if (now - lastMsg > 5000)
+  {
     lastMsg = now;
-
-    // Temperature in Celsius
     temperature = mpu6050.getTemp();
-    // Uncomment the next line to set temperature in Fahrenheit
-    // (and comment the previous temperature line)
-    // temperature = 1.8 * bme.readTemperature() + 32; // Temperature in Fahrenheit
-
-    // Convert the value to a char array
     char tempString[8];
     dtostrf(temperature, 1, 2, tempString);
     Serial.print("Temperature: ");
     Serial.println(tempString);
+    // Sending the temperature to the MQTT server
     client.publish("esp32/temperature", tempString);
   }
 }
-
